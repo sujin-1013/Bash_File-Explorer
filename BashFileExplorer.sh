27b4681b66424c81634868d80c203777f1d5849d
SeungHo0422 <songseungho9258@gmail.com>
2020-05-10 02:45:11 +0900

    left semicompleted -- need to item sort

--- BashFileExplorer.sh
+++ BashFileExplorer.sh
@@ -4,11 +4,19 @@
 # Last Update : 2020. 05. 07
 
 #Colors
-declare RED='\033[0;31m'
-declare SBLUEB='\033[0;30;46m' #skyblue bg with black fg
-declare GRAYB='\033[0;30;47m'
+#fg(..;**;..) : 30-black / 31-red / 32-green / 33-brown / 34-blue
+#bg(..;..;**) : 40-black / 41-red / 42-green / 43-brown / 44-blue / 46-skyblue / 47-gray
+
+#foreground with gray background
+declare RED='\033[0;31;47m'
+declare BLUE='\033[0;34;47m'
+declare GREEN='\033[0;32;47m'
 declare BLACK='\033[0;30m'
 declare NC='\033[0m'
+
+#background with black foreground
+declare GRAYB='\033[0;30;47m'
+declare SBLUEB='\033[0;30;46m' #skyblue bg with black fg
 
 #Cursors
 declare UP=[A
@@ -95,14 +103,54 @@ function Current_pwd() {
 
 #Print Current Path
 function Print_Left() {
-    tput cup 3 1
-    list=`ls -a`
-    printf "${GRAYB}${list}${NC}"
+    declare -i index
+    declare -i temp
+    for (( i=3;i<=22;i++))
+    do
+        temp=$i
+        index=temp-1
+        var=`ls -alh | tr -s " " | sort -k 9 | head -"${index}" | tail -1`
+        lvar=`ls -alh | tr -s " " | sort -k 9 | tail -1`
+        findvar=`ls -alh | tr -s " " | sort -k 9 | head -"${index}" | tail -1 | cut -d " " -f 9`
+        fvar="${PWD}/${findvar}"
+        lastlist="${PWD}/${lvar}"
+        tput cup $i 1 #file list - name
+        #coloring
+        if [ -d "${fvar}" ]; then #for Directories
+            printf "${BLUE}"
+        elif [ -x "${fvar}" ]; then #for Exectue files
+            printf "${GREEN}"
+        else
+            printf "${GRAYB}" #for normal files
+        fi
+
+        echo "${var}" | cut -d " " -f 9
+
+        tput cup $i 18 #file list - access
+        echo "${var}" | cut -d " " -f 1
+
+        tput cup $i 35 #file list - file size
+        if [ $index -eq 2 ] || [ $index -eq 3 ]
+        then
+            echo "-"
+        else
+            echo "${var}" | cut -d " " -f 5
+        fi
+
+        printf "${NC}" # End of default fg color
+        
+        if [ "${var}" = "${lvar}" ]; then
+            break;
+        fi
+    done
 }
 
 # Statement bar
 function Statement() {
-    tput cup 24 1
+    tput cup 24 5
+    tput cup 24 17
+    tput cup 24 
+    tput cup
 }
 
 #Main Command
